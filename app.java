97ef91977cae65bdecb56048b2bc5c6b8436c928
rajatrehal <114432152+rajatrehal@users.noreply.github.com>
2022-12-11 20:06:27 +0530

    Add files via upload

---
+++ app.java
@@ -0,0 +1,51 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+
+        // Initial code on Day 0
+        // Initial code on Day 0
+        // Initial code on Day 0
+        // Initial code on Day 0
+        // Initial code on Day 0
+        // Initial code on Day 0
+        // Initial code on Day 0
+        // Initial code on Day 0
+        // Initial code on Day 0
+
+        // This is Swagatams code
+
+        // Heavy day at Startup. Work of Day1. 
+        // Heavy day at Startup. Work of Day1. 
+        // Heavy day at Startup. Work of Day1. 
+        // Heavy day at Startup. Work of Day1. 
+        // Heavy day at Startup. Work of Day1. 
+        // Heavy day at Startup. Work of Day1. 
+        // Heavy day at Startup. Work of Day1. 
+
+        // code by kalbe hussain rizvi
+        // code by kalbe hussain rizvi
+        // code by kalbe hussain rizvi
+        // code by kalbe hussain rizvi 
+        // code by kalbe hussain rizvi
+        // code by kalbe hussain rizvi
+
+        //tried code on github
+        //tried code on github
+        //tried code on github
+        //tried code on github
+        //tried code on github
+        //tried code on github
+        //tried code on github
+        //tried code on github
+
+        //code by Rajat Kumar Rehal
+        //code by Rajat Kumar Rehal
+        //code by Rajat Kumar Rehal
+        //code by Rajat Kumar Rehal
+        //code by Rajat Kumar Rehal
+        //code by Rajat Kumar Rehal
+
+
+    }
+}
+// System.out.println("hello World");
