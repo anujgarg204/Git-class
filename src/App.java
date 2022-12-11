b96b9e0426558a88e4954fca6675ed6f9576f794
Anuj Garg <anuj@Anujs-MacBook-Air.local>
2022-12-11 18:49:49 +0530

    Adding App.java to commit

---
+++ src/App.java
@@ -0,0 +1,15 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+
+        // Initial code on Day 0
+        // Initial code on Day 0
+        // Initial code on Day 0
+        // Initial code on Day 0
+        // Initial code on Day 0
+        // Initial code on Day 0
+        // Initial code on Day 0
+        // Initial code on Day 0
+        // Initial code on Day 0
+    }
+}
